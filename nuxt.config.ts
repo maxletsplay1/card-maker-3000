7c29037c88cc112354b1cf1b56eb2c0bee7ba5aa
maksik <123letsplay93@gmail.com>
2025-11-27 01:14:06 +0700

    FEAT: добавление пакетов + конфигурация nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  ssr: false,
 })
